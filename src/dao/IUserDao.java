b65c3ac5e6eb4757444356c80a19391c216f2cc8
Paweł Bolek <pawel.bolek@student.uj.edu.pl>
2018-04-07 13:39:17 +0200

    git ignore

--- src/dao/IUserDao.java
+++ src/dao/IUserDao.java
@@ -1,5 +1,8 @@
 package dao;
 
 public interface IUserDao {
-
+    boolean isUserExist(String username);
+    boolean isUserPasswordCorrect(String username, String password);
+    String getUserNameById(int id);
+    boolean setUserPassword(String password);
 }
