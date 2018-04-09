2afd34d89e2ed5a12bc9a79d2c8e64758ca789d3
Paweł Bolek <pawel.bolek@student.uj.edu.pl>
2018-04-09 18:57:33 +0200

    controllers changes

--- src/controllers/LoginStageController.java
+++ src/controllers/LoginStageController.java
@@ -1,27 +1,69 @@
 package controllers;
 
-import javafx.application.Platform;
+import daoImpl.UserDao;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import utils.Strings;
+import utils.Utils;
+
+import java.io.IOException;
 
 public class LoginStageController {
     public TextField usernameField;
     public PasswordField passwordField;
+    public Text errorText;
 
     public void loginButton(ActionEvent actionEvent) {
 
+        UserDao userDao = new UserDao();
+        String username = usernameField.getText();
+        String pass = passwordField.getText();
 
-        //sql request
+        if(username.length() > 5 ) {
+            if(pass.length() > 5) {
+                if (userDao.isUserExist(username)) {
+                    if(userDao.isUserPasswordCorrect(username, pass)){
+                        errorText.setText("");
+                        //ok - start hub stage
+                        Parent root = null;
+                        try {
+                            root = FXMLLoader.load(getClass().getResource(Utils.getLayoutFolder() + "hub_stage.fxml"));
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        Stage newStage = new Stage();
+                        newStage.setTitle("Hub Stage");
+                        newStage.setScene(new Scene(root, 450, 450));
+                        newStage.show();
+                        //close this stage
+                        final Node source = (Node) actionEvent.getSource();
+                        final Stage stage = (Stage) source.getScene().getWindow();
+                        stage.close();
 
-        final Node source = (Node) actionEvent.getSource();
-        final Stage stage = (Stage) source.getScene().getWindow();
-        stage.close();
+                    } else {
+                        errorText.setText(Strings.WRONG_PASSWORD);
+                        // wrong password
+                    }
+                } else {
+                    errorText.setText(Strings.USER_NOT_EXIST);
 
-        //
+                    // user not exist
+                }
+            }else{
+                errorText.setText(Strings.PASSWORD_TOO_SHORT);
 
-        
+                // password to short
+            }
+        }else{
+            errorText.setText(Strings.WRONG_USERNAME);
+            // username too short
+        }
     }
 }
